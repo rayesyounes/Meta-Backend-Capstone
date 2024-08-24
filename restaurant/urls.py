7b360825634c68fda6a06d06bac96d8200a1c2d4
rayesyounes <rayesyounes88@gmail.com>
2024-08-24 20:13:01 +0100

    setup jwtAuth

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('api/token/login', TokenObtainPairView.as_view()),
+    path('api/token/refresh', TokenRefreshView.as_view()),
 ]
