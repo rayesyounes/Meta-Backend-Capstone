e04ebbcba9ffd66b5722e5fc06ddbecff77b1612
rayesyounes <rayesyounes88@gmail.com>
2024-08-31 14:43:15 +0100

    bugs fix / new features

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.utils import timezone
 from django.urls import reverse
-import requests, json, os
+from django.views import View
+from .forms import BookingForm
+import requests, json
 
 import environ
 
@@ -41,3 +43,42 @@ def menu_item(request, pk):
     headers = get_auth_header()
     response = requests.get(url, headers=headers)
     return render(request, 'restaurant/menu_item.html', context={'menu_item': json.loads(response.text)})
+
+
+class Book(View):
+    form_class = BookingForm
+    template_name = 'restaurant/book.html'
+
+    def get(self, request):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+
+    def get_data_from_form(self, form):
+        data = {
+            'name': form.cleaned_data.get('name'),
+            'no_of_guests': form.cleaned_data.get('no_of_guests'),
+            'booking_date': form.cleaned_data.get('booking_date'),
+        }
+        return data
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            data = self.get_data_from_form(form)
+            headers = get_auth_header()
+            url = f"http://127.0.0.1:8000{reverse('api:bookings')}"
+            response = requests.post(url, data=data, headers=headers)
+            if response.status_code == 201:
+                return render(request, 'restaurant/book.html', context={'form': BookingForm()})
+        return render(request, 'restaurant/book.html', context={'form': form})
+
+
+def bookings(request):
+    if request.GET.get('date') is None:
+        date = timezone.now().date()
+    else:
+        date = request.GET.get('date')
+    url = f"http://127.0.0.1:8000{reverse('api:bookings')}?date={date}"
+    headers = get_auth_header()
+    response = requests.get(url, headers=headers)
+    return render(request, 'restaurant/bookings.html', context={'bookings': response.text})
