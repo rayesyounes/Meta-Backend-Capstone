28252317d6dc5ec72a8f6e6145783bc70c0b8c9d
rayesyounes <rayesyounes88@gmail.com>
2024-08-31 10:23:19 +0100

    more tests

--- config/tests/tests.py
+++ config/tests/tests.py
@@ -3,7 +3,8 @@ from django.urls import reverse
 
 from api.serializers import MenuSerializer, BookingSerializer
 from restaurant.models import Menu, Booking
-from config.tests.mixins import UserMixin, MenuItemMixin, SingleMenuItemMixin, BookingMixin
+from config.tests.mixins import UserMixin, MenuItemMixin, SingleMenuItemMixin, BookingMixin, SingleBookingMixin
+import json
 
 
 class SetUpMixin:
@@ -20,17 +21,31 @@ class SetUpMixin:
         self.client = Client(HTTP_AUTHORIZATION=f'JWT {self.token}')
 
 
-class BookingTest(SetUpMixin, UserMixin, BookingMixin, TestCase):
+class BookingViewTest(SetUpMixin, UserMixin, BookingMixin, TestCase):
 
     def setUp(self):
         self.create_bookings()
         return super().setUp()
 
-    def test_get(self):
+    def test_list(self):
         response = self.client.get(reverse('api:bookings'))
         serializer = BookingSerializer(Booking.objects.all(), many=True)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data, serializer.data)
+
+    def test_create(self):
+        data = {'name': 'pete', 'no_of_guests': 4, 'booking_date': '2023-03-04'}
+        response = self.client.post(reverse('api:bookings'), data=data)
+        serializer = BookingSerializer(Booking.objects.get(name='pete'))
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.data, serializer.data)
+
+
+class SingleBookingViewTest(SetUpMixin, UserMixin, SingleBookingMixin, TestCase):
+
+    def setUp(self):
+        self.create_booking()
+        return super().setUp()
 
 
 class MenuItemViewTest(SetUpMixin, UserMixin, MenuItemMixin, TestCase):
@@ -64,3 +79,31 @@ class SingleMenuItemViewTest(SetUpMixin, UserMixin, SingleMenuItemMixin, TestCas
         serializer = MenuSerializer(Menu.objects.get(pk=self.menu_item.pk))
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data, serializer.data)
+
+    def test_partial_update(self):
+        data = json.dumps({'price': 3.99, 'inventory': 3})
+        response = self.client.patch(
+            reverse('api:menu-detail', kwargs={'pk': self.menu_item.pk}),
+            data=data,
+            content_type='application/json',
+        )
+        serializer = MenuSerializer(Menu.objects.get(pk=self.menu_item.pk))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, serializer.data)
+
+    def test_update(self):
+        data = json.dumps({'title': 'Apple Juice', 'price': 3.85, 'inventory': 7})
+        response = self.client.put(
+            reverse('api:menu-detail', kwargs={'pk': self.menu_item.pk}),
+            data=data,
+            content_type='application/json',
+        )
+        serializer = MenuSerializer(Menu.objects.get(pk=self.menu_item.pk))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, serializer.data)
+
+    def test_delete(self):
+        response = self.client.delete(reverse('api:menu-detail', kwargs={'pk': self.menu_item.pk}))
+        self.assertEqual(response.status_code, 204)
+        self.assertEqual(response.data, None)
+        self.assertEqual(Menu.objects.filter(pk=self.menu_item.pk).exists(), False)
